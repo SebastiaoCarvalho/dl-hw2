5dab11eee3cbdd5bd7152e6faa6cca7e0902c9d7
Sebastião Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-01-06 19:42:58 +0000

    some changes to final version and zip script

--- zip.sh
+++ zip.sh
@@ -1,6 +1,6 @@
 mkdir group1
 cp report/report.pdf group1
 mkdir group1/skeleton_code
-cp skeleton_code/*.py group1/skeleton_code
+cp skeleton_code/*.py skeleton_code/*.ipynb group1/skeleton_code
 zip -r group1.zip group1
 rm -rf group1
